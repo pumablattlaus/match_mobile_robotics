0adea61fd48a6c771e0e37fc1b85adb30b1a1253
--global <--global>
2022-08-23 17:36:11 +0200

    improvements but still not working properly

--- ur/ur_controllers_match/scripts/cartesian_controller.py
+++ ur/ur_controllers_match/scripts/cartesian_controller.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from ntpath import join
 import sys
 import copy
 import rospy
@@ -31,39 +32,61 @@ class JacobianPublisher():
             print(e)
             raise e
             
-        self.pub = rospy.Publisher(ns+"/joint_group_vel_controller/command", Float64MultiArray, queue_size=1)
+        self.pub = rospy.Publisher(ns+"/joint_group_vel_controller/command", Float64MultiArray, queue_size=10)
         rospy.Subscriber(ns+"/joint_states",JointState, self.joint_state_cb)
+        
         rospy.Subscriber(ns+"/arm_target_vel",Twist, self.target_vel_cb)
-        self.run()
-        rospy.spin()
+        # self.run()
+        # rospy.spin()
         
             
     def joint_state_cb(self,joint_states):
     
         joint_states_array=[joint_states.position[0],joint_states.position[1],joint_states.position[2],joint_states.position[3],joint_states.position[4],joint_states.position[5]]
         J_ur=self.group.get_jacobian_matrix(joint_states_array)
-        inverse = numpy.linalg.inv(J_ur) #TODO: try except
+        try:
+            inverse = numpy.linalg.inv(J_ur)
+        except numpy.linalg.LinAlgError as e:
+            rospy.loginfo(e)
+            rospy.loginfo(f"J_ur = {J_ur}")
+            # inverse = numpy.identity(6)
+            return
         target_dq = inverse.dot(self.target_vel)
-        
-        for i in range(0,len(target_dq)):
-            if abs(target_dq[i]) > 0.1:
-                target_dq[i] = target_dq[i]/abs(target_dq[i]) * 0.1 
+        # rospy.loginfo(f"target_dq = {target_dq}")
+        max_dq = abs(max(target_dq, key=abs))
+        if max_dq > 0.1:
+            for i in range(0,len(target_dq)):
+                target_dq[i] = target_dq[i]/max_dq * 0.1
+            rospy.loginfo(f"target_dq = {target_dq}")
+            rospy.loginfo(f"restricted_dq = {target_dq}")
+
         
         self.joint_vel.data = target_dq
+        self.pub.publish(self.joint_vel)
     
     def target_vel_cb(self,target_vel=Twist()):
         self.target_vel = numpy.array([target_vel.linear.x, target_vel.linear.y, target_vel.linear.z, target_vel.angular.x, target_vel.angular.y, target_vel.angular.z])
 
         
     def run(self):
-        rate = rospy.Rate(100)
-        while not rospy.is_shutdown():
-            self.pub.publish(self.joint_vel)
+        # rate = rospy.Rate(100)
+        # while not rospy.is_shutdown():
+        #     self.pub.publish(self.joint_vel)
             #pass 
             # print(self.joint_vel)
-            
+        rospy.spin()
         self.joint_vel.data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.pub.publish(self.joint_vel)
+
+    def save_shutdown(self):
+        # self.js_sub.unregister()
+        rospy.loginfo("Save shutdown")
+        joint_vel = Float64MultiArray()
+        joint_vel.data=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.joint_vel=joint_vel
+        self.pub.publish(self.joint_vel)
+        self.pub.unregister() # no addional commands possible
+        # rospy.signal_shutdown("save_shutdown")
         
     def config(self):
         self.target_vel = numpy.array([-0.0, -0.0, -0.0, 0.0, 0.0, 0.0])
@@ -71,4 +94,6 @@ class JacobianPublisher():
     
     
 if __name__ == '__main__':
-    JacobianPublisher()
+    cart=JacobianPublisher()
+    rospy.on_shutdown(cart.save_shutdown)
+    rospy.spin()
