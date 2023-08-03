287bbbde9f29a9c48cda63fea5d92bb75225266c
Hauke <heeren@match.uni-hannover.de>
2023-08-03 16:07:21 +0200

    fixed false reference

--- setup_github.sh
+++ setup_github.sh
@@ -1,7 +1,6 @@
-cd src/match_mobile_robotics
 git submodule update --init --recursive
 cd submodules/match_path_planning/splined_voronoi/nlopt/
 cmake .
 make
 sudo make install
-cd ../../../../../..
+cd ../../../..
