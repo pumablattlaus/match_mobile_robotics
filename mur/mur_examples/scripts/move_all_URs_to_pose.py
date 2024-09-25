6d41a048652f79b5b19ad72f02cbd7689443264f
Tobias Recker <Recker@match.uni-hannover.de>
2024-09-25 18:23:11 +0200

    improved performance

--- mur/mur_examples/scripts/move_all_URs_to_pose.py
+++ mur/mur_examples/scripts/move_all_URs_to_pose.py
@@ -29,7 +29,7 @@ for robot_name in robot_names:
 
     # Move the right arm to the target pose
     right_arm_group.set_named_target(target_pose_name)
-    right_arm_group.go(wait=False)
+    right_arm_group.go(wait=True)
 
 # Shutdown the moveit_commander module
 moveit_commander.roscpp_shutdown()
