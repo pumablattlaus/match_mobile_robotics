a46317c70f8d2244da2c23c8b534daeca63611b9
Tobias Recker <Recker@match.uni-hannover.de>
2024-09-27 16:09:41 +0200

    removed unused code

--- mur/mur_control/scripts/twist_controller_sim.py
+++ mur/mur_control/scripts/twist_controller_sim.py
@@ -12,10 +12,8 @@ class TwistControllerSim():
 
     def config(self):
         self.UR_joint_names = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
-        self.UR_prefix = rospy.get_param('~UR_prefix', 'UR10_l/')
         self.rate = rospy.get_param('~rate', 20)
         self.robot_name = rospy.get_param('~robot_name', 'mur620a')
-        self.joint_state_topic = rospy.get_param('~joint_state_topic', '/mur620a/joint_states')
         self.group_vel_controller_topic = rospy.get_param('~group_vel_controller_topic', '/mur620a/joint_group_vel_controller_l/unsafe/command')
         self.commanded_twist_topic = rospy.get_param('~commanded_twist_topic', '/mur620a/UR10_l/commanded_twist')
         self.move_group_name = rospy.get_param('~move_group_name', 'UR_arm_l')
@@ -23,51 +21,23 @@ class TwistControllerSim():
 
     def __init__(self) -> None:
         self.config()
-        # Define the DH parameters for the UR10e robot
-        self.a = [0, -0.612, -0.5723, 0, 0, 0]
-        self.alpha = [np.pi/2, 0, 0, np.pi/2, -np.pi/2, 0]
-        self.d = [ 0.1807, 0, 0, 0.17415, 0.11985,  0.11655]
-        #self.d = [0.1273, 0, 0, 0.163941, 0.1157, 0.0922]
-        self.theta = [0, 0, 0, 0, 0, 0]
         self.commanded_twist  = Twist()
         self.last_command_time = rospy.Time.now()
 
-
         # Initialize the moveit_commander module
         moveit_commander.roscpp_initialize([])
-        # wait until moveit is ready
-
-        rospy.logerr("Waiting for moveit to be ready")
-        rospy.logerr("Robot name: " + self.robot_name)
-        rospy.logerr("Move group name: " + self.move_group_name)
         robot = moveit_commander.RobotCommander(robot_description="/"+ self.robot_name + '/robot_description')
 
         self.group = moveit_commander.MoveGroupCommander(self.move_group_name , robot_description="/" + self.robot_name + '/robot_description')
 
-
-        rospy.Subscriber(self.joint_state_topic, JointState, self.joint_state_callback)
-        rospy.Subscriber(self.commanded_twist_topic, Twist, self.commanded_twist_callback)
-
-        rospy.wait_for_message(self.joint_state_topic, JointState)
-        rospy.loginfo('Received joint states')
-
         # initialize the publisher for the joint velocities
-        
         self.group_vel_controller_pub = rospy.Publisher(self.group_vel_controller_topic, Float64MultiArray, queue_size=1)
-
-
-
 
     # Compute the transformation matrix from the base frame to the end-effector frame
     def compute_jacobian(self):
         # get joint values from move_group
-        joint_values = self.group.get_current_joint_values()
-        # print(joint_values)
-        # print(self.joint_states)
-        # get jacobain matrix from move_group
-        
+        joint_values = self.group.get_current_joint_values()       
         J = self.group.get_jacobian_matrix(joint_values)
-        #print(J)
 
         return J
     
@@ -84,13 +54,6 @@ class TwistControllerSim():
         joint_velocities = np.linalg.pinv(jacobian) @ desired_twist
 
         return joint_velocities
-
-
-    def joint_state_callback(self, msg):
-        # get the correct joint states by name
-        self.joint_states = []
-        for joint_name in self.UR_joint_names:
-            self.joint_states.append(msg.position[msg.name.index(self.UR_prefix + "/" + joint_name)])
         
 
     def run(self):
